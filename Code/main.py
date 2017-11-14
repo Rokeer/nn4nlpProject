2262fb00d8af3c67fbac144381010eb0a04d7ffb
Colin Zhang <me@colinzhang.com>
2017-11-14 15:48:13 -0500

    Try 200

--- Code/main.py
+++ Code/main.py
@@ -65,8 +65,8 @@ def read_train(configuration):
             if int(end) >= len(sent_context):
                 print ("Wrong:" + ID)
             yield (sent_context, sent_question, sent_answers, context, question, answer, start, end, cx, cq)
-            # if lineindex >= 10:
-            #     break
+            if lineindex >= 200:
+                break
     config.MaxSentenceLength = max_length
     config.MaxQuestionLength = max_Query_Length
 
