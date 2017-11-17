057fb3f31e8c33bbeb565f36f900a4c655a19ba8
tazin-afrin <tazin.tumpa@gmail.com>
2017-11-16 20:25:35 -0500

    read 5000 samples

--- Code/main.py
+++ Code/main.py
@@ -127,8 +127,8 @@ def read_dev(configuration):
             if ID == '56cec3e8aab44d1400b88a02':
                 continue
             yield (sent_context, sent_question, sent_answers, context, question, answer, start, end, cx, cq, ID)
-            #if lineindex >= 50:
-            #    break
+            if lineindex >= 5000:
+                break
     #config.MaxSentenceLength = max_length
     #config.MaxQuestionLength = max_Query_Length
 
